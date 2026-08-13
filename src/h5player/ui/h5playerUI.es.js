82227eeb1b6d1ad8b032a280ff82cf0a29fb02f3
HX(Only Apple Can Do) <57313137+bgzo@users.noreply.github.com>
2026-08-13 21:51:41 +0800

    build: regenerate h5playerUI.es.js with toggleDownloadCachedVideo

--- src/h5player/ui/h5playerUI.es.js
+++ src/h5player/ui/h5playerUI.es.js
@@ -4313,7 +4313,7 @@ const h5playerUI = function (window) {var h5playerUI = (function () {
             {
               title: i18n.t('downloadCachedVideo'),
               desc: i18n.t('downloadCachedVideoDesc'),
-              action: 'downloadCachedVideo'
+              action: 'toggleDownloadCachedVideo'
             }
           ]
         },
